9394dcf46da180d83ff6e2e6b689a1fabea80a9e
Philip Makedonski <philip.makedonski@gmail.com>
2013-11-11 05:09:33 +0100

    + tst

--- src-gen/famix/ui/contentassist/antlr/PartialM3xContentAssistParser.java
+++ src-gen/famix/ui/contentassist/antlr/PartialM3xContentAssistParser.java
@@ -15,6 +15,7 @@ import org.eclipse.xtext.util.PolymorphicDispatcher;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
+@SuppressWarnings("restriction")
 public class PartialM3xContentAssistParser extends M3xParser implements IPartialContentAssistParser {
 
 	private AbstractRule rule;
